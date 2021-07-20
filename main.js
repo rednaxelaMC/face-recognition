d67a54111aa306a4f3661e2709a4555fa64cc235
rednaxelaMC <76746691+rednaxelaMC@users.noreply.github.com>
2021-07-20 12:52:29 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,22 @@ function take_snapshot(){
         document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>'; });   
 }
 console.log("ml5 version",ml5.version);
-classifer=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/EYAfDQUJA/model.json",modelLoaded);
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/EYAfDQUJA/model.json",modelLoaded);
 function modelLoaded(){
     console.log(modelLoaded)
+}
+function check(){
+    var img=document.getElementById('selfie_image');
+classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+         document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+
 }
