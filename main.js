6a4489f929d47bc5498fdca3c6fc266c03afe8e0
rednaxelaMC <76746691+rednaxelaMC@users.noreply.github.com>
2021-07-20 12:31:54 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+Webcam.attach('#camera');
+var camera=document.getElementById("camera");
+Webcam.set({
+width:350,
+height:300,
+image_format:'png',
+png_quality:90
+});
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>'; });   
+}
+console.log("ml5 version",ml5.version);
+classifer=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/EYAfDQUJA/model.json",modelLoaded);
+function modelLoaded(){
+    console.log(modelLoaded)
+}
